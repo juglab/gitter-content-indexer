f07e91cd47f8a28b4752b5ff919414cf2b09a31a
Gabriella Turek <turek@mpi-cbg.de>
2020-06-05 15:33:24 +0200

    Finalizing script. Need to add git actions to it

--- gitter_content_indexer.py
+++ gitter_content_indexer.py
@@ -9,24 +9,27 @@ import pprint
 import os
 import time
 import uuid
-import sys
+import tqdm
 
 
 import requests
 import requests_cache
 requests_cache.install_cache('gitter')
 from elasticsearch import Elasticsearch
-es = Elasticsearch()
+from elasticsearch.helpers import streaming_bulk
+
+client = Elasticsearch('http://localhost:9200')
 
 with open('token') as f:
     token = f.read().strip()
 
 h = {'Authorization': 'Bearer %s' % token}
+archive_dir = 'archive'
 
 def utcnow():
     return datetime.now(timezone.utc)
 
-def api_request(path):
+def gitter_api_request(path):
     request_time = utcnow()
     if not path.startswith('/'):
         path = '/' + path
@@ -45,74 +48,119 @@ def api_request(path):
         print("cached")
     return r.json()
 
-# Get list current list of groups:
+def create_index(client):
+    """Creates an index in Elasticsearch if one isn't already there."""
+    client.indices.create(
+        index='gitter-index',
+        body={
+            "settings": {"number_of_shards": 1},
+            "mappings": {
+                "properties": {
+                    "groupName": { "type": "keyword" },
+                    "roomName": { "type": "keyword" },
+                    "displayName": { "type": "text" },
+                    "username": { "type": "keyword" },
+                    "content": { "type": "text" },
+                    "sent": { "type": "date", "format": "date_optional_time" },
+                }
+            },
+        },
+        ignore=400,
+    )
+
+def extract_es_messages(messages):
+    for message in messages:
+        es_message = { 
+            'groupName' : group_name,
+            'roomName' : room_name,
+            'displayName' : message['fromUser']['displayName'],
+            'username' : message['fromUser']['username'],
+            'content' : message['text'],
+            'sent' : message['sent']
+        }
+        print(es_message)
+        yield es_message
+
+# Get list of indexes and check that the Gitter index exists.
+# If it does not, create it
 r = requests.get('http://localhost:9200/_aliases')
 r.raise_for_status()
 pp = pprint.PrettyPrinter(indent=4, width=80, compact=False)
 pp.pprint(r.json())
-stored_groups = r.json().keys()
-print(stored_groups)
-# Get list current list of rooms for each group:
-with open('create_room_index.json') as json_file:
-    mapping = json.load(json_file)
-    settings='{ "persistent": { "action.auto_create_index": "true"}}'
-groups = api_request('/groups?_=%s' % uuid.uuid4().hex)
-for group in groups:
-    group_name = group['name']
-    if group_name not in stored_groups:
-        es.indices.create(index=group_name, ignore=400)
-        es.indices.put_settings(settings, group_name)
-        es.indices.put_mapping(index=group_name, body=mapping, doc_type='_doc', include_type_name=True)
+indexes = r.json().keys()
+if ( 'gitter-index' not in indexes):
+    print('Creating index "gitter-index"')
+    create_index(client)
 
+# Get the data from Gitter
+archive_name = 'archive'
+rooms = gitter_api_request('/rooms?_=%s' % uuid.uuid4().hex)
+
+for room in rooms:
+    name = room['name']
+    print('Processing: ' + name)
+    group_name = 'None'
+    room_name = ''
+    if '/' in name:
+        group_name = name.rsplit('/', 1)[0]
+        room_name = name.rsplit('/', 1)[1]
+        
+    # We do not archive one to one or private conversations (the latter could be configurable)
+    if room['oneToOne'] or room.get('security') == 'PRIVATE':
         continue
-    sys.exit()
-    group_id = group['id']
-    request = '/groups/' + group_id + '/rooms?_=%s'
-    rooms = api_request(request % uuid.uuid4().hex)
-    dirname = 'archive/'
-    for room in rooms:
-        name = room['name']
-        if room['oneToOne'] or room.get('security') == 'PRIVATE':
+    
+    uri = room.get('uri', room['url'].lstrip('/'))
+    dest = os.path.join(archive_name, uri + '.json')
+    if '/' in dest:
+        d = dest.rsplit('/', 1)[0]
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+    if os.path.exists(dest):
+        print("Checking for new messages: %s" % dest)
+        with open(dest) as f:
+            room_messages = json.load(f)
+    else:
+        print("New room: %s" % dest)
+        room_messages = []
+        
+    if room_messages:
+        key='afterId'
+        last_id = room_messages[-1]['id']
+        messages = gitter_api_request('/rooms/%s/chatMessages?afterId=%s&_=%s' % (
+            room['id'], room_messages[-1]['id'], uuid.uuid4().hex))
+    else:
+        key='beforeId'
+        try:
+            messages = gitter_api_request('/rooms/%s/chatMessages?limit=5000' % room['id'])
+        except Exception as e:
+            print("Failed to get messages for %s: %s" % (name, e))
             continue
-        uri = room.get('uri', room['url'].lstrip('/'))
-        print(name)
-        dest = os.path.join(dirname, uri + '.json')
-        if '/' in dest:
-            d = dest.rsplit('/', 1)[0]
-            if not os.path.exists(d):
-                os.makedirs(d)
 
-        if os.path.exists(dest):
-            print("Checking for new messages: %s" % dest)
-            with open(dest) as f:
-                room_messages = json.load(f)
+    print('Indexing messages...')
+    number_of_docs = 0
+    progress = tqdm.tqdm(unit="docs", total=number_of_docs)
+    successes = 0
+    for ok, action in streaming_bulk( client=client, index='gitter-index', actions=extract_es_messages(messages)):
+        progress.update(1)
+        successes += ok
+    print('Indexed %d/%d messages' % (successes, number_of_docs))
+
+    while messages:
+        if key == 'beforeId':
+            room_messages[:0] = messages
+            edge_message = messages[0]
         else:
-            print("New room: %s" % dest)
-            room_messages = []
-            if room_messages:
-                key = 'afterId'
-                last_id = room_messages[-1]['id']
-                # cache-bust first forward-request
-                messages = api_request('/rooms/%s/chatMessages?afterId=%s&_=%s' % (
-                room['id'], room_messages[-1]['id'], uuid.uuid4().hex))
-            else:
-                key='beforeId'
-            try:
-                messages = api_request('/rooms/%s/chatMessages?' % room['id'])
-            except Exception as e:
-                print("Failed to get messages for %s: %s" % (name, e))
-                continue
+            room_messages.extend(messages)
+            edge_message = messages[-1]
+        print(len(room_messages))
+        print(edge_message['sent'], edge_message['text'].split('\n', 1)[0])
+        messages = gitter_api_request('/rooms/%s/chatMessages?%s=%s' % (
+            room['id'], key, edge_message['id']))
+    
+    # Finally archive the messages
+    with open(dest, 'w') as f:
+        json.dump(room_messages, f, sort_keys=True, indent=1)
 
-        while messages:
-            if key == 'beforeId':
-                 # left-extend before
-                room_messages[:0] = messages
-                edge_message = messages[0]
-            else:
-                room_messages.extend(messages)
-                edge_message = messages[-1]
-            print(len(room_messages))
-            print(edge_message['sent'], edge_message['text'].split('\n', 1)[0])
-            messages = api_request('/rooms/%s/chatMessages?%s=%s' % (room['id'], key, edge_message['id']))
-        with open(dest, 'w') as f:
-            json.dump(room_messages, f, sort_keys=True, indent=1)
+    
+    
